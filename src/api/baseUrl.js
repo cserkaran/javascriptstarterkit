26e3e78807ed2aec2cbb03994956311905d7202a
Karan Sharma <sharmak@crossmuller.com.au>
2017-07-27 19:17:14 +1000

    Finished the course

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-    return getQueryStringParameterByName('useMockApi')  ?  'http://localhost:3001/' :  '/';
+    return getQueryStringParameterByName('useMockApi')  ?  'http://localhost:3001/' :  'https://thawing-headland-81260.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
